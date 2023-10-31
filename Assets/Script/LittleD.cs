4beb80f586b175da8d9859a89ea4446fbab25d51
Nattawut jukkrathok <142292660+BKkuma@users.noreply.github.com>
2023-10-31 21:27:51 +0700

    add Animation

--- Assets/Script/LittleD.cs
+++ Assets/Script/LittleD.cs
@@ -7,15 +7,31 @@ public class LittleD : MonoBehaviour
     [SerializeField] private  float speed = 1;
 
     Rigidbody2D rg2d;
+    Animator animator;
     float horizontalValue;
+    float runSpeedModidifier = 2;
+    bool isRuning = false;
+    bool facingRight = true;
+
     private void Awake()
     {
         rg2d = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
 
     }
     void Update()
     {
         horizontalValue = Input.GetAxisRaw("Horizontal");
+
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            isRuning = true;
+        }
+       else if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            isRuning = false;
+        }
+
     }
     private void FixedUpdate()
     {
@@ -23,8 +39,26 @@ public class LittleD : MonoBehaviour
     }
     void Move(float dir)
     {
-        float xVal = dir * speed * Time.deltaTime;
+        float xVal = dir * speed * 100 * Time.deltaTime;
+        if (isRuning)
+        {
+            xVal *= runSpeedModidifier;
+        }
         Vector2 targetVelocity = new Vector2(xVal, rg2d.velocity.y);
         rg2d.velocity = targetVelocity;
+
+        
+
+        if (facingRight && dir < 0)
+        {
+            transform.localScale = new Vector3(-1,1, 1);
+            facingRight = false;
+        }
+        else if (!facingRight && dir > 0)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+            facingRight = true;
+        }
+        animator.SetFloat("xVelocity",Mathf.Abs(rg2d.velocity.x));
     }
 }
